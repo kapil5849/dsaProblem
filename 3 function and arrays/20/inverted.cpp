037cdb6ee8506ec28ba41683924ceb8e570591b4
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-01-03 08:17:59 +0530

    code revision
    code revision

--- 3 function and arrays/20/inverted.cpp
+++ 3 function and arrays/20/inverted.cpp
@@ -13,7 +13,7 @@ int main(){
             max = arr[i];
         }
     }
-    for(int i=max;i>=1;i--){
+    for(int i=1;i<=max;i++){
         for(int j=0;j<n;j++){
             if(arr[j]>=i){
                 cout<<"*\t";
--- only.cpp
+++ only.cpp
@@ -1,23 +1,25 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
     int arr[n];
-    for(int i=0;i<n;i++) cin>>arr[i];
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
     int max = arr[0];
     for(int i=1;i<n;i++){
         if(arr[i]>max){
             max = arr[i];
         }
     }
-    for(int i=max;i>=1;i--){
+    for(int i=1;i<=max;i++){
         for(int j=0;j<n;j++){
             if(arr[j]>=i){
-                cout<<"* ";
+                cout<<"*\t";
             }else{
-                cout<<"  ";
-        }
+                cout<<"\t";
+            }
         }
         cout<<endl;
     }
